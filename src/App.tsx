fbef74dab0ae1c15520f8b15f83e233c583a86cb
knm22mukk <knm22mukk@gmail.com>
2022-07-10 11:47:55 +0900

    useStateを使ってみました

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import List from './List';
 
-const App: FC = () => (
-  <div>
-    ゼロから始めるReact入門 ts編
-    <List title='取扱言語一覧' />
-  </div>
-);
+const App: FC = () => {
+  const [description, setDescription] = useState<string>('クリック前の表示');
+
+  const changeDescription = (): void => {
+    setDescription('クリックしました');
+  };
+
+  return (
+    <div>
+      {description}
+      <List title='取扱言語一覧' />
+      <button type='button' onClick={changeDescription}>
+        ボタン
+      </button>
+    </div>
+  );
+};
 
 export default App;
