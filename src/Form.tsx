a76748ac0fb2178dd9483f34e1fc406a2c4874ee
knm22mukk <knm22mukk@gmail.com>
2022-07-10 13:12:33 +0900

    フォームの利用、map関数のkeyの設定

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,5 +1,25 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-const Form: FC = () => <div>フォームです</div>;
+const Form: FC = () => {
+  const [text, setText] = useState<string>('');
+
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(text);
+  };
+  return (
+    <div>
+      <h4>新しい言語の追加</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+        <div>
+          <button type='submit'>追加</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
--- src/List.tsx
+++ src/List.tsx
@@ -1,12 +1,23 @@
 import { FC } from 'react';
 
-const LANGAGES: string[] = ['JavaScript', 'C++', 'Ruby', 'Java', 'PHP', 'Go'];
+type LANGUAGES = {
+  name: string[];
+};
+
+const LANGUAGES = [
+  { id: 0, name: 'JavaScript' },
+  { id: 1, name: 'C++' },
+  { id: 2, name: 'Ruby' },
+  { id: 3, name: 'Java' },
+  { id: 4, name: 'PHP' },
+  { id: 5, name: 'Go' },
+];
 
 const List: FC = () => (
   <div>
     <div>
-      {LANGAGES.map((lang) => (
-        <div>{lang}</div>
+      {LANGUAGES.map((lang) => (
+        <div key={lang.id}>{lang.name}</div>
       ))}
     </div>
   </div>
